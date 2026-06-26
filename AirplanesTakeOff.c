4f275078cc17daf1e305a92dd28ed8ee2928c5ff
Fovir <fovir@disroot.org>
2026-06-26 16:12:27 +0800

    refactor: refactor code
    Signed-off-by: Fovir <fovir@disroot.org>

--- AirplanesTakeOff.c
+++ AirplanesTakeOff.c
@@ -3,23 +3,67 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 #include <unistd.h>
+
+#define PLANE_NUMBER 3
+#define SAFETY_INSPECTION_DURATION 20
+#define PASSENGER_BOARDING_DURATION 25
+#define TAXI_OUT_DURATION 20
+#define PLANE_NAME_LENGTH 50
 
 sem_t semaphore;
 pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct {
-    char name[50];
+    char name[PLANE_NAME_LENGTH];
     bool safty_inspection;
     bool passengers_boarding;
     bool taxi_out; // The taxiing process to the runway
     pthread_t take_off;
 } Airplane;
 
-void initializeAirplne(Airplane * plane, int i) {
-    sprintf(plane->name, "airplane%d", i);
+void initialize_airplane(Airplane * plane, int id);
+void * preparation(void * args);
+void * runway(void * args);
+int random_number_in_range(int min, int max);
+
+int main() {
+    sem_init(&semaphore, 0, 1); // Initialize semaphore
+    srand((unsigned int)time(NULL));
+
+    Airplane plane[PLANE_NUMBER];
+    for (int i = 0; i < PLANE_NUMBER; i++) {
+        initialize_airplane(&plane[i], i + 1);
+    }
+
+    for (int i = 0; i < PLANE_NUMBER; i++) {
+        pthread_create(&plane[i].take_off, NULL, &preparation, &plane[i]);
+    }
+
+    for (int i = 0; i < PLANE_NUMBER; i++) {
+        pthread_join(plane[i].take_off, NULL);
+    }
+
+    printf("Airplanes finish preparation simultaneously,"
+           "they will all simultaneously send a request to"
+           "the control tower to use runway\n");
+
+    for (int i = 0; i < PLANE_NUMBER; i++) {
+        pthread_create(&plane[i].take_off, NULL, &runway, &plane[i]);
+    }
+
+    for (int i = 0; i < PLANE_NUMBER; i++) {
+        pthread_join(plane[i].take_off, NULL);
+    }
+
+    sem_destroy(&semaphore);
+    pthread_mutex_destroy(&print_mutex);
+    return 0;
+}
+
+void initialize_airplane(Airplane * plane, int id) {
+    sprintf(plane->name, "airplane%d", id);
     plane->safty_inspection = false;
     plane->passengers_boarding = false;
     plane->taxi_out = false;
@@ -30,28 +74,28 @@ void * preparation(void * args) {
     pthread_mutex_lock(&print_mutex);
     printf("%s finishes safety inspection.\n", p->name);
     pthread_mutex_unlock(&print_mutex);
-    usleep(200);
+    sleep(SAFETY_INSPECTION_DURATION);
 
     pthread_mutex_lock(&print_mutex);
     printf("%s finishes passengers boarding.\n", p->name);
     pthread_mutex_unlock(&print_mutex);
-    usleep(250);
+    sleep(PASSENGER_BOARDING_DURATION);
 
     pthread_mutex_lock(&print_mutex);
     printf("%s finishes taxi-out.\n", p->name);
     pthread_mutex_unlock(&print_mutex);
-    usleep(200);
+    sleep(TAXI_OUT_DURATION);
 
     return NULL;
 }
-void * runway(void * args) {
 
+void * runway(void * args) {
     sem_wait(&semaphore);
     printf("-----------------------------------\n");
     Airplane * p = (Airplane *)args;
     printf("%s are taking off.\n", p->name);
 
-    int random_num = (rand() % (10 - 5 + 1)) + 5;
+    int random_num = random_number_in_range(5, 10);
     sleep(random_num);
 
     printf("%s finished. Time taken %d mins\n", p->name, random_num);
@@ -61,30 +105,6 @@ void * runway(void * args) {
     return NULL;
 }
 
-int main() {
-    sem_init(&semaphore, 0, 1); // Initialize semaphore
-    srand((unsigned int)time(NULL));
-
-    Airplane plane[3];
-    for (int i = 0; i < 3; i++)
-        initializeAirplne(&plane[i], i + 1);
-
-    for (int i = 0; i < 3; i++)
-        pthread_create(&plane[i].take_off, NULL, &preparation, &plane[i]);
-
-    for (int i = 0; i < 3; i++)
-        pthread_join(plane[i].take_off, NULL);
-
-    printf("Airplanes finish preparation simultaneously,"
-           "they will all simultaneously send a request to"
-           "the control tower to use runway\n");
-
-    for (int i = 0; i < 3; i++)
-        pthread_create(&plane[i].take_off, NULL, &runway, &plane[i]);
-    for (int i = 0; i < 3; i++)
-        pthread_join(plane[i].take_off, NULL);
-
-    sem_destroy(&semaphore);
-    pthread_mutex_destroy(&print_mutex);
-    return 0;
+int random_number_in_range(int min, int max) {
+    return min + rand() % (max - min + 1);
 }
